08b2b3c607d830fc343e916bf8841137620c6afa
Ayush Shrivastava <makeporium@gmail.com>
2026-04-12 16:01:21 +0530

    basic code done

--- app/src/main/java/com/example/mad/MainActivity.java
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.mad;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.mad.fragments.CalendarFragment;
+import com.example.mad.fragments.CommunityFragment;
+import com.example.mad.fragments.EventsFragment;
+import com.example.mad.fragments.HomeFragment;
+import com.example.mad.fragments.InfoHubFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +21,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView urgentHelpButton = findViewById(R.id.tvUrgentHelp);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        urgentHelpButton.setOnClickListener(v -> {
+            // Hook emergency action here (call, SOS screen, etc.).
         });
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                loadFragment(new HomeFragment());
+                return true;
+            } else if (itemId == R.id.nav_community) {
+                loadFragment(new CommunityFragment());
+                return true;
+            } else if (itemId == R.id.nav_calendar) {
+                loadFragment(new CalendarFragment());
+                return true;
+            } else if (itemId == R.id.nav_info_hub) {
+                loadFragment(new InfoHubFragment());
+                return true;
+            } else if (itemId == R.id.nav_events) {
+                loadFragment(new EventsFragment());
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
     }
 }
